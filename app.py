01d81b1c518b3c103c0a04064326411a5f44034b
varun raj <varunrajtonk@gmail.com>
2025-01-27 02:55:58 +0530

    deploy changes

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ from nltk.stem.porter import PorterStemmer
 
 ps = PorterStemmer() 
 
-
+nltk.download('punkt')
 def transform_text(text):
     text = text.lower()
     text = nltk.word_tokenize(text)
